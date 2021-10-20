0bfd60dc17ee66fdad4973dd86b7df1df6439cda
sunwenpeng.0601 <sunwenpeng.0601@bytedance.com>
2021-10-20 21:10:22 +0800

    Update `

--- variadic.cc
+++ variadic.cc
@@ -32,38 +32,55 @@ struct PrintTraits
     using Type = PrintTraitsHelper<i>;
 };
 
-template <int... Is>
-struct IntVector
-{
-    using Type = IntVector<Is...>;
-};
+// template <int... Is>
+// struct IntVector
+// {
+//     using Type = IntVector<Is...>;
+// };
 
-template <int I_Size, typename T_Vector = IntVector<>>
-struct Iota;
-template <int I_Size, int... Is>
-struct Iota<I_Size, IntVector<Is...>> : Iota<I_Size - 1, IntVector<I_Size - 1, Is...>>
-{
-};
-template <int... Is>
-struct Iota<0, IntVector<Is...>> : IntVector<Is...>
-{
-};
+// template <int I_Size, typename T_Vector = IntVector<>>
+// struct Iota;
+// template <int I_Size, int... Is>
+// struct Iota<I_Size, IntVector<Is...>> : Iota<I_Size - 1, IntVector<I_Size - 1, Is...>>
+// {
+// };
+// template <int... Is>
+// struct Iota<0, IntVector<Is...>> : IntVector<Is...>
+// {
+// };
 
-template <typename T_Indexes>
+// template <typename T_Indexes>
+// struct PrintTable;
+// template <int... Is>
+// struct PrintTable<IntVector<Is...>>
+// {
+//     static void at(const char in)
+//     {
+//         static const std::function<void()> table[] = {typename PrintTraits<Is>::Type{}...};
+//         table[in]();
+//     }
+// };
+
+template <typename T>
 struct PrintTable;
-template <int... Is>
-struct PrintTable<IntVector<Is...>>
+template <size_t... N>
+struct PrintTable<std::index_sequence<N...>>
 {
-    static void at(const char in)
+    static void at(const int in)
     {
-        static const std::function<void()> table[] = {typename PrintTraits<Is>::Type{}...};
+        static const std::function<void()> table[] = {typename PrintTraits<N>::Type{}...};
         table[in]();
     }
 };
 
 void PrintInt(int c)
 {
-    using Table = PrintTable<typename Iota<999>::Type>;
+    // using Table = PrintTable<typename Iota<99999>::Type>;
+    constexpr int size = 2048;
+    if (c > size)
+        return;
+
+    using Table = PrintTable<std::make_index_sequence<size>>;
     Table::at(c);
 }
 
