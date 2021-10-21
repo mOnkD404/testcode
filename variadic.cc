efa8f9ee1e2e8f59e2d658cd3704b6927f2e602e
sunwenpeng.0601 <sunwenpeng.0601@bytedance.com>
2021-10-22 00:49:53 +0800

    Update

--- variadic.cc
+++ variadic.cc
@@ -2,31 +2,53 @@
 #include <cmath>
 #include <iostream>
 
-template <int... Is>
+template <char... Is>
 struct DigitsVector {
   using Type = DigitsVector<Is...>;
-  static void PrintDigitsString() { ((std::cout << Is << " "), ...); }
-  static void PrintDigitsCount() { std::cout << sizeof...(Is); }
+  enum { DigitsCount = sizeof...(Is) };
+
+  static void PrintDigitsString() { ((std::cout << (int)Is << " "), ...); }
+  static void PrintDigitsCount() { std::cout << DigitsCount << " "; }
+
+  constexpr const static char* NameTable[] = {
+      "ones digit", "tens digit", "hundreds digit", "thousands digit",
+      "ten thousands digit"};
+  static_assert(sizeof...(Is) <= sizeof(NameTable), "e");
+
+  inline constexpr static void PrintDigitsNameHelper(size_t index, char c) {
+    std::cout << NameTable[index - 1] << " : " << (int)c << " ";
+  }
+  template <typename... Ps>
+  inline constexpr static void PrintDigitsNameHelper(size_t index,
+                                                     char c,
+                                                     Ps&&... args) {
+    std::cout << NameTable[index - 1] << " : " << (int)c << " ";
+    PrintDigitsNameHelper(index - 1, std::forward<Ps>(args)...);
+  }
+
+  static void PrintDigitsStringName() {
+    PrintDigitsNameHelper(sizeof...(Is), Is...);
+  }
 };
 
 template <int IntVal, typename = DigitsVector<>>
 struct DigitTraitsHelper;
 
-template <int IntVal, int... Is>
+template <int IntVal, char... Is>
 struct DigitTraitsHelper<IntVal, DigitsVector<Is...>>
     : DigitTraitsHelper<IntVal / 10, DigitsVector<IntVal % 10, Is...>> {};
 
 template <int... Is>
 struct DigitTraitsHelper<0, DigitsVector<Is...>> : DigitsVector<Is...> {};
 
-template <typename T, int... Rs>
+template <typename T, char... Rs>
 struct DigitVectorReverseHelper;
 
-template <int IntVal, int... Is, int... Rs>
+template <int IntVal, char... Is, char... Rs>
 struct DigitVectorReverseHelper<DigitsVector<IntVal, Is...>, Rs...>
     : DigitVectorReverseHelper<DigitsVector<Is...>, IntVal, Rs...> {};
 
-template <int... Rs>
+template <char... Rs>
 struct DigitVectorReverseHelper<DigitsVector<>, Rs...> : DigitsVector<Rs...> {};
 
 template <int i>
@@ -38,11 +60,20 @@ struct PrintTraits {
   void operator()() {
     std::cout << " total: ";
     Helper::PrintDigitsCount();
-    std::cout << " digits: ";
+    // std::cout << " digits: ";
+    Helper::PrintDigitsStringName();
+    std::cout << " in order: ";
     Helper::PrintDigitsString();
     std::cout << " reverse: ";
     ReverseHelper::PrintDigitsString();
     std::cout << std::endl;
+  }
+};
+
+template <>
+struct PrintTraits<0> {
+  void operator()() {
+    std::cout << " total: 1 ones digit: 0 in order: 0 reverse: 0 " << std::endl;
   }
 };
 
@@ -57,7 +88,7 @@ struct PrintTable<std::index_sequence<N...>> {
 };
 
 void PrintInt(int c) {
-  constexpr int size = 1000;
+  constexpr int size = 100;
   if (c >= size)
     return;
 
